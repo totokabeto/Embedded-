2c3837f79beb684aae8d249c25ef99e14a82ecf8
totokabeto <phamgiakhanh20@gmail.com>
2023-06-09 17:57:51 +0700

    new

---
+++ B9/BaiTapT4.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void tinhToan(char arr[], int arrSize){
+for (int i = 0; i < arrSize; i++)
+{
+    if(arr[i] == '+'){
+
+    }
+}
+
+}
+
+int main(int argc, char const *argv[])
+{
+    char arr[] ={" 8 + 4 + 3 +2 "};
+    return 0;
+}
