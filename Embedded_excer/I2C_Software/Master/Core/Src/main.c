595e46b5658054519055813640e2061049229f49
totokabeto <phamgiakhanh20@gmail.com>
2023-08-04 17:25:34 +0700

    i2c

--- Embedded_excer/I2C_Software/Master/Core/Src/main.c
+++ Embedded_excer/I2C_Software/Master/Core/Src/main.c
@@ -18,7 +18,7 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-
+#include "stm32f1xx_hal.h"
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 
@@ -56,15 +56,172 @@ static void MX_GPIO_Init(void);
 void i2c_init(void);
 void i2c_start(void);
 void i2c_stop(void);
-void i2c_write(uint8_t data);
-uint8_t i2c_read(uint8_t ack);
+void i2c_write(uint8_t);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+void i2c_init(void)
+{
+  // Khởi tạo các chân GPIO cho I2C
+  GPIO_InitTypeDef GPIO_InitStruct;
 
+  GPIO_InitStruct.Pin = I2C_PIN_SCL | I2C_PIN_SDA;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(I2C_GPIO_PORT, &GPIO_InitStruct);
+
+  // Đặt giá tr�? ban đầu cho các chân GPIO
+  HAL_GPIO_WritePin(I2C_GPIO_PORT, I2C_PIN_SCL | I2C_PIN_SDA, GPIO_PIN_SET);
+}
+
+void i2c_start(void)
+{
+  // Đặt chân SDA th�?nh đầu ra v�? đặt giá tr�? HIGH
+  GPIO_InitTypeDef GPIO_InitStruct;
+
+  GPIO_InitStruct.Pin = I2C_PIN_SDA;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(I2C_GPIO_PORT, &GPIO_InitStruct);
+
+  HAL_GPIO_WritePin(I2C_GPIO_PORT, I2C_PIN_SDA, GPIO_PIN_SET);
+  HAL_GPIO_WritePin(I2C_GPIO_PORT, I2C_PIN_SCL, GPIO_PIN_SET);
+
+  // Đợi một chút trước khi ghi giá tr�? LOW v�?o chân SDA
+  HAL_Delay(1);
+
+  HAL_GPIO_WritePin(I2C_GPIO_PORT, I2C_PIN_SDA, GPIO_PIN_RESET);
+
+  // Đợi một chút trước khi ghi giá tr�? LOW v�?o chân SCL
+  HAL_Delay(1);
+
+  HAL_GPIO_WritePin(I2C_GPIO_PORT, I2C_PIN_SCL, GPIO_PIN_RESET);
+}
+
+void i2c_stop(void)
+{
+  // Đặt chân SDA th�?nh đầu ra v�? đặt giá tr�? LOW
+  GPIO_InitTypeDef GPIO_InitStruct;
+
+  GPIO_InitStruct.Pin = I2C_PIN_SDA;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(I2C_GPIO_PORT, &GPIO_InitStruct);
+
+  HAL_GPIO_WritePin(I2C_GPIO_PORT, I2C_PIN_SDA, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(I2C_GPIO_PORT, I2C_PIN_SCL, GPIO_PIN_SET);
+
+  // Đợi một chút trước khi đặt giá tr�? HIGH v�?o chân SDA
+  HAL_Delay(1);
+
+  HAL_GPIO_WritePin(I2C_GPIO_PORT, I2C_PIN_SDA, GPIO_PIN_SET);
+}
+
+void i2c_write(uint8_t data)
+{
+  // Ghi từng bit của dữ liệu v�?o chân SDA
+  for (uint8_t i = 0; i < 8; i++) {
+    if ((data & (1 << (7 - i))) != 0) {
+      HAL_GPIO_WritePin(I2C_GPIO_PORT, I2C_PIN_SDA, GPIO_PIN_SET);
+    } else {
+      HAL_GPIO_WritePin(I2C_GPIO_PORT, I2C_PIN_SDA, GPIO_PIN_RESET);
+    }
+
+    // Đợi một chút trước khi ghi giá tr�? HIGH v�?o chân SCL
+    HAL_Delay(1);
+
+    HAL_GPIO_WritePin(I2C_GPIO_PORT, I2C_PIN_SCL, GPIO_PIN_SET);
+
+    // Đợi một chút trước khi ghi giá tr�? LOW v�?o chân SCL
+    HAL_Delay(1);
+
+    HAL_GPIO_WritePin(I2C_GPIO_PORT, I2C_PIN_SCL, GPIO_PIN_RESET);
+  }
+
+  // Đợi một chút trước khi đọc giá tr�? ACK từ chân SDA
+  HAL_Delay(1);
+
+  // Đặt chân SDA th�?nh đầu v�?o v�? đọc giá tr�? ACK
+  GPIO_InitTypeDef GPIO_InitStruct;
+
+  GPIO_InitStruct.Pin = I2C_PIN_SDA;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(I2C_GPIO_PORT, &GPIO_InitStruct);
+
+  HAL_GPIO_WritePin(I2C_GPIO_PORT, I2C_PIN_SCL, GPIO_PIN_SET);
+
+  // Đợi một chút trước khi đọc giá tr�? ACK từ chân SDA
+  HAL_Delay(1);
+
+  uint8_t ack = HAL_GPIO_ReadPin(I2C_GPIO_PORT, I2C_PIN_SDA);
+
+  HAL_GPIO_WritePin(I2C_GPIO_PORT, I2C_PIN_SCL, GPIO_PIN_RESET);
+
+  // Trả v�? giá tr�? ACK
+  return ack == GPIO_PIN_RESET;
+}
+
+uint8_t i2c_read(uint8_t ack)
+{
+  uint8_t data = 0;
+
+  // Đặt chân SDA th�?nh đầu v�?o v�? đọc từng bit của dữ liệu
+  GPIO_InitTypeDef GPIO_InitStruct;
+
+  GPIO_InitStruct.Pin = I2C_PIN_SDA;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(I2C_GPIO_PORT, &GPIO_InitStruct);
+
+  for (uint8_t i = 0; i < 8; i++) {
+    HAL_GPIO_WritePin(I2C_GPIO_PORT, I2C_PIN_SCL, GPIO_PIN_SET);
+
+    // Đợi một chút trước khi đọc giá tr�? từ chân SDA
+    HAL_Delay(1);
+
+    data |= (HAL_GPIO_ReadPin(I2C_GPIO_PORT, I2C_PIN_SDA) << (7 - i));
+
+    HAL_GPIO_WritePin(I2C_GPIO_PORT, I2C_PIN_SCL, GPIO_PIN_RESET);
+
+    // Đợi một chút trước khi đọc giá tr�? từ chân SDA
+    HAL_Delay(1);
+  }
+
+  // Ghi ACK hoặc NACK v�?o chân SDA
+  GPIO_InitStruct.Pin = I2C_PIN_SDA;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+    HAL_GPIO_Init(I2C_GPIO_PORT, &GPIO_InitStruct);
+
+    if (ack) {
+      HAL_GPIO_WritePin(I2C_GPIO_PORT, I2C_PIN_SDA, GPIO_PIN_RESET);
+    } else {
+      HAL_GPIO_WritePin(I2C_GPIO_PORT, I2C_PIN_SDA, GPIO_PIN_SET);
+    }
+
+    // Đợi một chút trước khi ghi giá tr�? ACK hoặc NACK v�?o chân SCL
+    HAL_Delay(1);
+
+    HAL_GPIO_WritePin(I2C_GPIO_PORT, I2C_PIN_SCL, GPIO_PIN_SET);
+
+    // Đợi một chút trước khi ghi giá tr�? ACK hoặc NACK v�?o chân SCL
+    HAL_Delay(1);
+
+    HAL_GPIO_WritePin(I2C_GPIO_PORT, I2C_PIN_SCL, GPIO_PIN_RESET);
+
+    // Trả v�? dữ liệu đọc được
+    return data;
+}
 /* USER CODE END 0 */
 
 /**
@@ -80,23 +237,24 @@ int main(void)
   /* MCU Configuration--------------------------------------------------------*/
 
   /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
-  i2c_init();
-  /* USER CODE BEGIN Init */
 
+	HAL_Init();
+
+
+  /* USER CODE BEGIN Init */
+	SystemClock_Config();
   /* USER CODE END Init */
 
   /* Configure the system clock */
-  SystemClock_Config();
-
   /* USER CODE BEGIN SysInit */
 
   /* USER CODE END SysInit */
 
   /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  /* USER CODE BEGIN 2 */
 
+  /* USER CODE BEGIN 2 */
+	MX_GPIO_Init();
+	i2c_init();
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -104,13 +262,17 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-	/*  i2c_start();
-	  i2c_write(0x27);
-	  i2c_write(0x01);
-	  i2c_write(0x02);
-	  i2c_stop();*/
-	  HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_10);
-	  HAL_Delay(100);
+	 /* for (uint8_t i = 1; i <= 100; i++) {
+	        i2c_start();
+	        i2c_write(0x27);
+	        i2c_write(i);
+	        i2c_stop();
+
+	        // Đợi một chút trước khi gửi số tiếp theo
+	        HAL_Delay(10);
+	      }*/
+	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_10, GPIO_PIN_SET);
+	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_11, GPIO_PIN_SET);
     /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
@@ -159,180 +321,40 @@ void SystemClock_Config(void)
   */
 static void MX_GPIO_Init(void)
 {
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
 /* USER CODE BEGIN MX_GPIO_Init_1 */
 /* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5|GPIO_PIN_6, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_10|GPIO_PIN_11, GPIO_PIN_RESET);
 
   /*Configure GPIO pins : PA5 PA6 */
+  GPIO_InitStruct.Pin = GPIO_PIN_5|GPIO_PIN_6;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
+  /*Configure GPIO pins : PB10 PB11 */
+  GPIO_InitStruct.Pin = GPIO_PIN_10|GPIO_PIN_11;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 /* USER CODE BEGIN MX_GPIO_Init_2 */
 /* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
-void i2c_init(void)
-{
-  // Khởi tạo các chân GPIO cho I2C
-  GPIO_InitTypeDef GPIO_InitStruct;
 
-  GPIO_InitStruct.Pin = I2C_PIN_SCL | I2C_PIN_SDA;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-  HAL_GPIO_Init(I2C_GPIO_PORT, &GPIO_InitStruct);
-
-  // Đặt giá trị ban đầu cho các chân GPIO
-  HAL_GPIO_WritePin(I2C_GPIO_PORT, I2C_PIN_SCL | I2C_PIN_SDA, GPIO_PIN_SET);
-}
-
-void i2c_start(void)
-{
-  // Đặt chân SDA thành đầu ra và đặt giá trị HIGH
-  GPIO_InitTypeDef GPIO_InitStruct;
-
-  GPIO_InitStruct.Pin = I2C_PIN_SDA;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-  HAL_GPIO_Init(I2C_GPIO_PORT, &GPIO_InitStruct);
-
-  HAL_GPIO_WritePin(I2C_GPIO_PORT, I2C_PIN_SDA, GPIO_PIN_SET);
-  HAL_GPIO_WritePin(I2C_GPIO_PORT, I2C_PIN_SCL, GPIO_PIN_SET);
-
-  // Đợi một chút trước khi ghi giá trị LOW vào chân SDA
-  HAL_Delay(1);
-
-  HAL_GPIO_WritePin(I2C_GPIO_PORT, I2C_PIN_SDA, GPIO_PIN_RESET);
-
-  // Đợi một chút trước khi ghi giá trị LOW vào chân SCL
-  HAL_Delay(1);
-
-  HAL_GPIO_WritePin(I2C_GPIO_PORT, I2C_PIN_SCL, GPIO_PIN_RESET);
-}
-
-void i2c_stop(void)
-{
-  // Đặt chân SDA thành đầu ra và đặt giá trị LOW
-  GPIO_InitTypeDef GPIO_InitStruct;
-
-  GPIO_InitStruct.Pin = I2C_PIN_SDA;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-  HAL_GPIO_Init(I2C_GPIO_PORT, &GPIO_InitStruct);
-
-  HAL_GPIO_WritePin(I2C_GPIO_PORT, I2C_PIN_SDA, GPIO_PIN_RESET);
-  HAL_GPIO_WritePin(I2C_GPIO_PORT, I2C_PIN_SCL, GPIO_PIN_SET);
-
-  // Đợi một chút trước khi đặt giá trị HIGH vào chân SDA
-  HAL_Delay(1);
-
-  HAL_GPIO_WritePin(I2C_GPIO_PORT, I2C_PIN_SDA, GPIO_PIN_SET);
-}
-
-void i2c_write(uint8_t data)
-{
-  // Ghi từng bit của dữ liệu vào chân SDA
-  for (uint8_t i = 0; i < 8; i++) {
-    if ((data & (1 << (7 - i))) != 0) {
-      HAL_GPIO_WritePin(I2C_GPIO_PORT, I2C_PIN_SDA, GPIO_PIN_SET);
-    } else {
-      HAL_GPIO_WritePin(I2C_GPIO_PORT, I2C_PIN_SDA, GPIO_PIN_RESET);
-    }
-
-    // Đợi một chút trước khi ghi giá trị HIGH vào chân SCL
-    HAL_Delay(1);
-
-    HAL_GPIO_WritePin(I2C_GPIO_PORT, I2C_PIN_SCL, GPIO_PIN_SET);
-
-    // Đợi một chút trước khi ghi giá trị LOW vào chân SCL
-    HAL_Delay(1);
-
-    HAL_GPIO_WritePin(I2C_GPIO_PORT, I2C_PIN_SCL, GPIO_PIN_RESET);
-  }
-
-  // Đợi một chút trước khi đọc giá trị ACK từ chân SDA
-  HAL_Delay(1);
-
-  // Đặt chân SDA thành đầu vào và đọc giá trị ACK
-  GPIO_InitTypeDef GPIO_InitStruct;
-
-  GPIO_InitStruct.Pin = I2C_PIN_SDA;
-  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-  HAL_GPIO_Init(I2C_GPIO_PORT, &GPIO_InitStruct);
-
-  HAL_GPIO_WritePin(I2C_GPIO_PORT, I2C_PIN_SCL, GPIO_PIN_SET);
-
-  // Đợi một chút trước khi đọc giá trị ACK từ chân SDA
-  HAL_Delay(1);
-
-  uint8_t ack = HAL_GPIO_ReadPin(I2C_GPIO_PORT, I2C_PIN_SDA);
-
-  HAL_GPIO_WritePin(I2C_GPIO_PORT, I2C_PIN_SCL, GPIO_PIN_RESET);
-
-  // Trả về giá trị ACK
-  return ack == GPIO_PIN_RESET;
-}
-
-uint8_t i2c_read(uint8_t ack)
-{
-  uint8_t data = 0;
-
-  // Đặt chân SDA thành đầu vào và đọc từng bit của dữ liệu
-  GPIO_InitTypeDef GPIO_InitStruct;
-
-  GPIO_InitStruct.Pin = I2C_PIN_SDA;
-  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-  HAL_GPIO_Init(I2C_GPIO_PORT, &GPIO_InitStruct);
-
-  for (uint8_t i = 0; i < 8; i++) {
-    HAL_GPIO_WritePin(I2C_GPIO_PORT, I2C_PIN_SCL, GPIO_PIN_SET);
-
-    // Đợi một chút trước khi đọc giá trị từ chân SDA
-    HAL_Delay(1);
-
-    data |= (HAL_GPIO_ReadPin(I2C_GPIO_PORT, I2C_PIN_SDA) << (7 - i));
-
-    HAL_GPIO_WritePin(I2C_GPIO_PORT, I2C_PIN_SCL, GPIO_PIN_RESET);
-
-    // Đợi một chút trước khi đọc giá trị từ chân SDA
-    HAL_Delay(1);
-  }
-
-  // Ghi ACK hoặc NACK vào chân SDA
-  GPIO_InitStruct.Pin = I2C_PIN_SDA;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-    HAL_GPIO_Init(I2C_GPIO_PORT, &GPIO_InitStruct);
-
-    if (ack) {
-      HAL_GPIO_WritePin(I2C_GPIO_PORT, I2C_PIN_SDA, GPIO_PIN_RESET);
-    } else {
-      HAL_GPIO_WritePin(I2C_GPIO_PORT, I2C_PIN_SDA, GPIO_PIN_SET);
-    }
-
-    // Đợi một chút trước khi ghi giá trị ACK hoặc NACK vào chân SCL
-    HAL_Delay(1);
-
-    HAL_GPIO_WritePin(I2C_GPIO_PORT, I2C_PIN_SCL, GPIO_PIN_SET);
-
-    // Đợi một chút trước khi ghi giá trị ACK hoặc NACK vào chân SCL
-    HAL_Delay(1);
-
-    HAL_GPIO_WritePin(I2C_GPIO_PORT, I2C_PIN_SCL, GPIO_PIN_RESET);
-
-    // Trả về dữ liệu đọc được
-    return data;
-}
 /* USER CODE END 4 */
 
 /**
